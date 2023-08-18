f2026acdd39cc93e045e11c06bfbd126a139f41c
Divesh Gole <goledivesh@gmail.com>
2023-08-18 21:26:47 +0530

    calculators page created

--- src/component/Layout/Footer.tsx
+++ src/component/Layout/Footer.tsx
@@ -1,17 +1,61 @@
 // https://flowbite.com/docs/components/footer/
 
+import Image from "next/image";
+import Link from "next/link";
+import { FaCopyright } from "react-icons/fa";
+import Container from "../Container";
+import { navItems } from "./Header";
+
 type Props = {};
 
 function Footer({}: Props) {
   return (
-    <footer className="p-4 bg-slate-900 text-center md:p-5">
-      <span className=" text-gray-400 sm:text-center">
-        © {new Date().getFullYear() }{" "}
-        <a href="https://Emininja.com" className="underline">
-          EmiNinja
-        </a>
-        . All Rights Reserved.
-      </span>
+    <footer className="relative bg-gray-900 text-gray-200">
+      <nav className="px-4 py-10 md:py-16">
+        <Container>
+          <div className="grid grid-cols-4 gap-8">
+            <div className="col-span-2">
+              <Link href="/" className="flex items-center">
+                <Image
+                  src="/images/header-logo.png"
+                  className="h-8 mr-3 object-contain"
+                  alt="Emi Ninja"
+                  width={34}
+                  height={34}
+                />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap">
+                  EmiNinja
+                </span>
+              </Link>
+
+              <p className="mt-5 leading-7 max-w-sm text-gray-200">
+                Welcome to EmiNinja Financial Calculators! Empower your
+                financial decisions with our suite of easy-to-use and accurate
+                financial calculators.
+              </p>
+            </div>
+
+            <div>
+              {navItems.map(({ name, url }) => {
+                return (
+                  <p key={name}>
+                    <Link href={url} className='text-gray-200 hover:text-blue-500'>{name}</Link>
+                  </p>
+                );
+              })}
+            </div>
+
+            <div></div>
+          </div>
+        </Container>
+      </nav>
+      <nav className="p-4 bg-gray-800 text-center md:p-5">
+        <span className="flex items-center justify-center  sm:text-center">
+          <FaCopyright className="mr-1" /> 2022 - {new Date().getFullYear()}{" "}
+          EmiNinja. All Rights Reserved, Built with{" "}
+          <span className="mx-2">❤️</span> in India
+        </span>
+      </nav>
     </footer>
   );
 }
